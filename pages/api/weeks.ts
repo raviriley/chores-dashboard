169223d84ccac4eb1d518f48da5a56771c4c502e
Ravi Riley <raviriley@gmail.com>
2023-11-27 17:06:02 -0800

    Add try-catch to weeks endpoint

--- pages/api/weeks.ts
+++ pages/api/weeks.ts
@@ -25,27 +25,35 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const file = await fs.readFile(
-    process.cwd() + "/../parker-chores-bot/weeks.json",
-    "utf8",
-  );
-  const data: WeekInput = JSON.parse(file);
+  try {
+    const file = await fs.readFile(
+      process.cwd() + "/../parker-chores-bot/weeks.json",
+      "utf8",
+    );
+    const data: WeekInput = JSON.parse(file);
 
-  // transform weeks.json into an array of Week objects
-  const result = [];
-  for (const [weekName, tasks] of Object.entries(data)) {
-    const weekNumber = parseInt(weekName.split(" ")[1]);
+    // transform weeks.json into an array of Week objects
+    const result = [];
+    for (const [weekName, tasks] of Object.entries(data)) {
+      const weekNumber = parseInt(weekName.split(" ")[1]);
 
-    for (const [name, details] of Object.entries(tasks)) {
-      const week: Week = {
-        week_number: weekNumber,
-        name: name,
-        task: details.task,
-        completed: details.completed,
-        current: false,
-      };
-      result.push(week);
+      for (const [name, details] of Object.entries(tasks)) {
+        const week: Week = {
+          week_number: weekNumber,
+          name: name,
+          task: details.task,
+          completed: details.completed,
+          current: false,
+        };
+        result.push(week);
+      }
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: `An unknown error occurred: ${error}` });
     }
   }
-  res.status(200).json(result);
 }
